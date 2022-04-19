161d5e619f723c6f1296c7e95a71b818afa9d513
czlingo <czlingo@gmail.com>
2022-04-19 09:41:30 +0800

    fix: ignore business error

--- pkg/broadcaster/v1beta1/client.go
+++ pkg/broadcaster/v1beta1/client.go
@@ -1,10 +1,18 @@
 package v1beta1
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"reflect"
 
 	"github.com/quanxiang-cloud/cabin/tailormade/client"
+	"github.com/quanxiang-cloud/cabin/tailormade/header"
+	"github.com/quanxiang-cloud/cabin/tailormade/resp"
 )
 
 type Client struct {
@@ -29,11 +37,51 @@ func New(c *client.Config, target string) *Client {
 	}
 }
 
-type Resp struct {
-}
+type Resp = resp.Resp
 
 func (c *Client) Send(ctx context.Context, fn Function) (*Resp, error) {
 	resp := &Resp{}
-	err := client.POST(ctx, c.client, c.target, fn, resp)
-	return resp, err
+	if err := client.POST(ctx, c.client, c.target, fn, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// POST http post
+func POST(ctx context.Context, client *http.Client, uri string, params interface{}, entity interface{}) error {
+	if reflect.ValueOf(entity).Kind() != reflect.Ptr {
+		return errors.New("the entity type must be a pointer")
+	}
+
+	paramByte, err := json.Marshal(params)
+	if err != nil {
+		return err
+	}
+
+	reader := bytes.NewReader(paramByte)
+	req, err := http.NewRequest("POST", uri, reader)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add(header.GetRequestIDKV(ctx).Wreck())
+	req.Header.Add(header.GetTimezone(ctx).Wreck())
+	req.Header.Add(header.GetTenantID(ctx).Wreck())
+
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected state value is 200, actually %d", response.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, entity)
 }
